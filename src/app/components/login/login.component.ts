49c03e7db50e2a3dba7675c789544f7fb5a8a38b
mayank <123.ultron.1234@gmail.com>
2025-05-17 21:56:23 +0530

    login form added

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,11 +1,23 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, inject } from '@angular/core';
+import { RouterLinkActive } from '@angular/router';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  private fb = inject(FormBuilder);
+    loginForm = this.fb.group({
+      username: ['',Validators.required],
+      password: ['',Validators.required]
+    });
+  errorMessage = '';
 
+  onSubmit(){
+    console.log('clicked', this.loginForm.getRawValue())
+  }
 }
