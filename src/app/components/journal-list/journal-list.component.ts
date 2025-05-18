e60c37bf26e471a0138f8a8fec6d24b750870e7b
mayank <123.ultron.1234@gmail.com>
2025-05-18 16:36:00 +0530

    component added

---
+++ src/app/components/journal-list/journal-list.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-journal-list',
+  imports: [],
+  templateUrl: './journal-list.component.html',
+  styleUrl: './journal-list.component.css'
+})
+export class JournalListComponent {
+
+}
