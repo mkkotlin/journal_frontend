8caa665ff08f67dd4389cfae4add8a6064f1c344
mayank <123.ultron.1234@gmail.com>
2025-05-20 16:05:48 +0530

    minor fixes

--- src/app/components/entry-form/entry-form.component.ts
+++ src/app/components/entry-form/entry-form.component.ts
@@ -14,15 +14,15 @@ export class EntryFormComponent {
     entryForm !: FormGroup;
     
     ngOnInit(){
+      this.loadEntries();
       const a = localStorage.getItem('access_token')
       console.log(a)
     this.entryForm = this.fb.group({
-      // user: [1,Validators.required],
       title: ['', Validators.required],
       mood: ['',Validators.required],
-      // is_private: ['true']
     });}
     successMessage = '';
+    errorMessage = '';
 
     constructor(private fb:FormBuilder, private entryService: EntryService, private auth: AuthService){}
 
@@ -30,8 +30,8 @@ export class EntryFormComponent {
       
       if (this.entryForm.valid){
         this.entryService.createEntries(this.entryForm.value).subscribe({
-          next: ()=> this.successMessage = 'Added',
-          error: ()=> this.successMessage = 'error'
+          next: ()=> {this.successMessage = 'Added', this.ngOnInit(), this.errorMessage = ''},
+          error: ()=> {this.errorMessage = 'Someting went wrong', this.successMessage=''}
         })
       }
     }
