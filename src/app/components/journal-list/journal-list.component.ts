289e92fefe91bb2c93834182addcc6c2c308308e
mayank <123.ultron.1234@gmail.com>
2025-05-21 13:51:50 +0530

    journal-list get()

--- src/app/components/journal-list/journal-list.component.ts
+++ src/app/components/journal-list/journal-list.component.ts
@@ -1,11 +1,24 @@
 import { Component } from '@angular/core';
+import { JournalListService } from '../../services/journal-list.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-journal-list',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './journal-list.component.html',
   styleUrl: './journal-list.component.css'
 })
 export class JournalListComponent {
+  constructor(private journalService: JournalListService, ){}
 
+  journalList: any[] = []
+  getList(){
+    this.journalService.getJournal().subscribe({
+      next: (res: any) => {this.journalList = res, console.log(this.journalList)},
+      error: err => {console.log(err)}
+    })
+  }
+  ngOnInit() {
+    this.getList()
+  }
 }
--- src/app/services/journal-list.service.ts
+++ src/app/services/journal-list.service.ts
@@ -1,9 +1,21 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class JournalListService {
 
-  constructor() { }
+  private apiUrl = 'http://127.0.0.1:8000/api/journals/';
+  constructor(private http: HttpClient) { }
+
+  getJournal():Observable<any>{
+    const token = localStorage.getItem('access_token');
+    const headers = new HttpHeaders({
+      'Authorization': `Bearer ${token}`
+    })
+    return this.http.get(this.apiUrl, {headers})
+  }
+
 }
