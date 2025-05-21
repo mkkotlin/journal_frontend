4f2f915e0b8554e2740134b7eec5772dbbf33ebd
mayank <123.ultron.1234@gmail.com>
2025-05-21 17:10:24 +0530

    book view componenet

---
+++ src/app/components/book-view/book-view.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-book-view',
+  imports: [],
+  templateUrl: './book-view.component.html',
+  styleUrl: './book-view.component.css'
+})
+export class BookViewComponent {
+
+}
