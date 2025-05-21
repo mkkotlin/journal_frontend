3d252ea5969a59341c17e28a24604ffbf51e8e7d
mayank <123.ultron.1234@gmail.com>
2025-05-21 18:09:47 +0530

    book list view

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
 import { JournalListComponent } from './components/journal-list/journal-list.component';
 import { EntryFormComponent } from './components/entry-form/entry-form.component';
+import { BookViewComponent } from './components/book-view/book-view.component';
 
 export const routes: Routes = [
     { path:'', redirectTo: 'login', pathMatch:'full'},
     { path: 'login', component: LoginComponent},
     { path: 'journal-list', component: JournalListComponent},
-    { path:'add-entry', component: EntryFormComponent,}
+    { path:'add-entry', component: EntryFormComponent,},
+    { path: 'book-view', component: BookViewComponent}
 ];
--- src/app/components/book-view/book-view.component.ts
+++ src/app/components/book-view/book-view.component.ts
@@ -1,11 +1,51 @@
 import { Component } from '@angular/core';
+import { EntryService } from '../../services/entry.service';
+import { JournalListService } from '../../services/journal-list.service';
+import { forkJoin } from 'rxjs';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-book-view',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './book-view.component.html',
   styleUrl: './book-view.component.css'
 })
 export class BookViewComponent {
+  // book: any ={}
+  error: string =''
+  constructor(private entryService:EntryService, private journalService: JournalListService){}
+
+  entry: any[]=[]
+  mood: any[]=[]
+  ngOnInit(){
+// this for only single value 
+    // forkJoin({
+    //   mood: this.entryService.getEntries(),
+    //   content: this.journalService.getJournal()
+    // }).subscribe({
+    //   next: (res)=>{
+    //     this.book = {
+    //       title: res.content[res.content.length-1].title,
+    //       content:res.content[res.content.length-1].content,
+    //       date: res.content[res.content.length-1].created_at,
+    //       mood:res.mood[res.mood.length-1].mood
+    //     }
+    //   , console.log(this.book,res);
+    //   }, error: (err)=>{this.error = 'Failed', console.log(this.error)}
+    // })
+
+    forkJoin({
+      mood: this.entryService.getEntries(),
+      entry: this.journalService.getJournal()
+    }).subscribe({
+      next: (res)=>{
+        this.mood = res.mood;
+        this.entry = res.entry;
+      }, error: (err) => {
+        this.error = 'Failed';
+        console.error(err)
+      }
+    })
+  }
 
 }
